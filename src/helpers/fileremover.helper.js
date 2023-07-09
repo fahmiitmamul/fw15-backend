03250e463a4cb9858a154a665f9e495ff7c20087
fahmipraga <itmamul2004@gmail.com>
2023-07-09 20:39:35 +0700

    fix: fileremover

--- src/helpers/fileremover.helper.js
+++ src/helpers/fileremover.helper.js
@@ -4,6 +4,7 @@ const fileRemover = async (file) => {
   if (file) {
     try {
       const filename = file.slice(8)
+      console.log(filename)
       await cloudinary.uploader.destroy(filename)
     } catch (err) {
       console.log(err)
