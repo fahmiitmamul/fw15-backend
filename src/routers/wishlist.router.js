fcc6103b25bab08a55aa19e0c17ad6e1911ea726
fahmipraga <itmamul2004@gmail.com>
2023-07-11 21:58:47 +0700

    fix: wishlist router

--- src/routers/wishlist.router.js
+++ src/routers/wishlist.router.js
@@ -2,7 +2,7 @@ const wishlistRouter = require("express").Router()
 const wishlistController = require("../controllers/wishlist.controller")
 const validate = require("../middlewares/validator.middleware")
 
-wishlistRouter.get("/", validate("getOne"), wishlistController.getWishlist)
+wishlistRouter.get("/:id", validate("getOne"), wishlistController.getWishlist)
 wishlistRouter.get("/all", validate("getOne"), wishlistController.getAllWishlist)
 wishlistRouter.post("/", validate("manageWishlist"), wishlistController.makeWishlist)
 wishlistRouter.delete("/:id", validate("Delete"), wishlistController.deleteWishlist)
